813925256f9b74bfe007bb7ea541794b9b02ba03
versatileisonline <versatileisonline@gmail.com>
2025-10-28 13:16:18 -0400

    for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ButtonAppBar from './components/Bar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     hello
